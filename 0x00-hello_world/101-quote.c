d6c9f7b192f581eca727690b295633257ac716e4
Ayjay7 <awa.nda.jiba@gmail.com>
2022-09-08 11:08:47 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+{#include <stdio.h>
 
 /**
- * main - prints script stated
+ * main - prints string function
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
 int main(void)
 {
 	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
-}
+}}
