aec655bd31aba8a9e98b53964253331866dce6e5
Ayjay7 <awa.nda.jiba@gmail.com>
2022-09-13 06:46:33 -0700

    completed tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,40 +10,25 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 10; b++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			c = b * a;
+			if (b == 0)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar (' ');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(',');
-				}
+				_putchar(c + '0');
 			}
-			else
+
+			if (c < 10 && b != 0)
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar (',');
-					_putchar (',');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
-
