902ff82780b59d939f1a8026707691ee15335d8d
Ayjay7 <awa.nda.jiba@gmail.com>
2022-09-15 05:02:45 -0700

    task done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; x < 58; i++)
+	for (i = 0; x < 10; i++)
 	{
-		if (i != 50 && i != 52)
+		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 
